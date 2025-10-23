b342413c3a5224150d4a1f68fe1c3622da2cfad8
Luminosa <cansycoskun@gmail.com>
2025-10-23 23:34:51 +0300

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,8 +1,8 @@
 import requests
 
 # API key and base URL
-api_key = "8a68ceaf41f1b671e63f3e6c"
-base_url = "https://v6.exchangerate-api.com/v6/8a68ceaf41f1b671e63f3e6c/latest/"
+api_key = "{your_api}"
+base_url = "https://v6.exchangerate-api.com/v6/{your_api}/latest/"
 
 # Get input from the user
 exchange_currency = input("Enter the currency you have (e.g., TRY) : ").upper()
@@ -25,7 +25,7 @@ try:
     if foreign_exchange_received in data.get("conversion_rates",{}):
         rate = data["conversion_rates"][foreign_exchange_received]
         new_amount = amount*rate
-        print(f"{amount}{exchange_currency} = {new_amount:.2f}{foreign_exchange_received}")
+        print(f"{amount} {exchange_currency} = {new_amount:.2f} {foreign_exchange_received}")
     else:
         print(f"Error:Conversion rate for '{foreign_exchange_received}' was not found.")
         exit()
@@ -33,4 +33,5 @@ try:
 except requests.exceptions.RequestException as x:
     print(f"API request failed:{x}")
 except KeyError:
+
     print("API response structure is incorrect or you entered an invalid currency code.")
